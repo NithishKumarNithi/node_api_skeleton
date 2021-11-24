43379d90a2c45822dcd7127e728f8a0bd4958733
unknown <nithishkumarnithi19@gmail.com>
2021-11-24 04:42:59 -0800

    added mongo uri in config.js

--- config.js
+++ config.js
@@ -3,3 +3,4 @@ const config = {};
 module.exports = config;
 
 config.PORT = 9999;
+config.MONGO_URI = "mongodb+srv://nodeapiskeleton:nodeapiskeleton123@nodeapiskeleton.px6yr.mongodb.net/nodeApiSkeleton?retryWrites=true&w=majority"
